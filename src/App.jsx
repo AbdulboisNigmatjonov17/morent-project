523016356fed1d410c3ebc92e12684dfc4577503
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-01-20 13:27:35 +0500

    data tarafini boshladik

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./Layout"
+import ErrorPage from "./pages/error/ErrorPage"
+import HomePage from "./pages/home/HomePage"
 
 const App = () => {
+
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      errorElement: <ErrorPage/>,
+      children: [
+        {
+          index: true,
+          element: <HomePage/>
+        }
+      ]
+    }
+  ])
+
   return (
-    <>
-      
-    </>
+    <RouterProvider router={routes} />
   )
 }
 
