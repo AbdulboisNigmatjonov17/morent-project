cebffdc36af0bca767f4b4a5953c27f1b116c1aa
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-01-20 16:07:48 +0500

    navbar qismi tugadi

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const logo = () => {
   return (
-    <a class="text-primary text-2xl font-semibold text-[#3563e9]" href="/">MORENT</a>
+    <a class="text-primary text-2xl font-semibold text-[#3563e9] bg-inherit" href="/">MORENT</a>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,28 +3,45 @@ import Logo from '../logo/logo'
 import TuneTwoToneIcon from '@mui/icons-material/TuneTwoTone';
 import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
 import SettingsTwoToneIcon from '@mui/icons-material/SettingsTwoTone';
+import NotificationsSharpIcon from '@mui/icons-material/NotificationsSharp';
+import FavoriteSharpIcon from '@mui/icons-material/FavoriteSharp';
 
 const Navbar = () => {
+
+  const user = 'https://morent-website.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FImageprofile.a9ab6a22.png&w=64&q=75'
+
   return (
     <>
-      <nav className="w-full h-[10vh] flex items-center">
-        <div className="leftSide flex gap-20 items-center max-w-[70%]">
+      <nav className="w-full h-[12vh] flex items-center justify-between bg-white px-5">
+        <div className="leftSide flex gap-20 items-center max-w-[70%] bg-inherit">
           <Logo />
-          <form className='border-[#c1c1c1] border-[1px] rounded-xl text-[20px] gap-20 px-4 py-2 max-w-screen-sm flex justify-between'>
-            <div className='flex gap-5'>
+          <form className='border-[#c1c1c1] bg-inherit border-[1px] rounded-xl text-[20px] gap-32 px-4 py-2 max-w-screen-sm flex justify-between'>
+            <div className='flex gap-5 bg-inherit'>
               <label htmlFor="search">
-                <SearchTwoToneIcon fontSize='large' className='cursor-pointer' />
+                <SearchTwoToneIcon fontSize='large' className='cursor-pointer bg-white opacity-50 hover:opacity-100' />
               </label>
-              <input type="text" id='search' placeholder='Search something here' className='focus:outline-none ' />
+              <input type="text" id='search' placeholder='Search something here' className=' focus:outline-none bg-white' />
             </div>
             <label htmlFor="search">
-              <TuneTwoToneIcon fontSize='large' className='cursor-pointer' />
+              <TuneTwoToneIcon fontSize='large' className='cursor-pointer bg-white opacity-50 hover:opacity-100' />
             </label>
           </form>
         </div>
-        <div className='rightSide'>
-          <div className="icon rounded-full border-[2px] p-1">
-            <SettingsTwoToneIcon fontSize='large' className='rounded-full'/>
+        <div className='rightSide max-w-[30%] flex justify-start bg-white'>
+          <div className=' flex w-full items-center gap-5 bg-inherit'>
+            <div className="icon rounded-full border-[#c1c1c1] border-[1px] bg-inherit p-2">
+              <SettingsTwoToneIcon className='rounded-full cursor-pointer' />
+            </div>
+            <div className="icon rounded-full border-[#c1c1c1] bg-inherit border-[1px] p-2">
+              <NotificationsSharpIcon className='rounded-full cursor-pointer' />
+            </div>
+            <div className="icon rounded-full border-[#c1c1c1] bg-inherit border-[1px] p-2">
+              <FavoriteSharpIcon className='rounded-full cursor-pointer' />
+            </div>
+            <div className="icon rounded-full cursor-pointer flex justify-center">
+              <img src={user} width={60} alt="uer-img" className='rounded-full transition ease-in-out delay-150 hover:scale-125' />
+            </div>
+
           </div>
         </div>
       </nav>
