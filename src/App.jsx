38d1fce7b50b2fb7cd65832bb20b30854bf03394
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-01-20 12:11:01 +0500

    boshlang'ich ishla bajarildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 const App = () => {
   return (
@@ -8,4 +7,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App 
---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const logo = () => {
+  return (
+    <a class="text-primary text-2xl font-semibold text-[#3563e9]" href="/">MORENT</a>
+  )
+}
+
+export default logo
