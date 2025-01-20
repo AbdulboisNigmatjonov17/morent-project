0977976f2e9c92507af142249aabec64e0e8b37d
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-01-20 16:16:02 +0500

    navbar qismida ozgina ozgartrish kiritdim

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,14 +16,14 @@ const Navbar = () => {
         <div className="leftSide flex gap-20 items-center max-w-[70%] bg-inherit">
           <Logo />
           <form className='border-[#c1c1c1] bg-inherit border-[1px] rounded-xl text-[20px] gap-32 px-4 py-2 max-w-screen-sm flex justify-between'>
-            <div className='flex gap-5 bg-inherit'>
-              <label htmlFor="search">
-                <SearchTwoToneIcon fontSize='large' className='cursor-pointer bg-white opacity-50 hover:opacity-100' />
+            <div className='flex gap-5 bg-white'>
+              <label htmlFor="search" className='opacity-50 hover:opacity-100'>
+                <SearchTwoToneIcon fontSize='large' className='cursor-pointer bg-white ' />
               </label>
               <input type="text" id='search' placeholder='Search something here' className=' focus:outline-none bg-white' />
             </div>
-            <label htmlFor="search">
-              <TuneTwoToneIcon fontSize='large' className='cursor-pointer bg-white opacity-50 hover:opacity-100' />
+            <label htmlFor="search" className='opacity-50 hover:opacity-100'>
+              <TuneTwoToneIcon fontSize='large' className='cursor-pointer bg-white' />
             </label>
           </form>
         </div>
