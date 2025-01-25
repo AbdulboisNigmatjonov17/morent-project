b562e2c3d4d8b12a5c36ebc60a0c7f2067ab668c
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-01-25 15:42:47 +0500

    azgina qoldi

---
+++ src/components/RentComponents/Step2of4.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const Step2of4 = () => {
+    return (
+        <div className='w-[50%] bg-white py-5 px-10 rounded-lg'>
+            <div className='w-full flex justify-between'>
+                <div>
+                    <h3 className='font-bold text-[20px]'>Rental Info</h3>
+                    <h2>Please eneter your billing info</h2>
+                </div>
+                <h3>Step 2 of 4</h3>
+            </div>
+            <form className='mt-5'>
+                <h1 className='text-[25px] font-semibold'>
+                    Pick - Up
+                </h1>
+                <div className=''>
+                    <div className='flex justify-between gap-5'> 
+                        <div className='flex flex-col'> 
+                            <label>Locations</label>
+                            <select id="" className='bg-[#eeee] w-[300px] py-3 px-5 rounded-lg' >
+                                <option value="">Toshkent</option>
+                                <option value="">Tokio</option>
+                                <option value="">Madrid</option>
+                            </select>
+                        </div>
+                        <div className='flex flex-col'>
+                            <label>Date</label>
+                            <input type="date" className='bg-[#eeee] w-[300px] py-3 px-5 rounded-lg' />
+                        </div>
+                    </div>
+                    <div className='flex flex-col'>
+                        <label>Time</label>
+                        <input type="time" className='bg-[#eeee] w-[300px] py-3 px-5 rounded-lg' />
+                    </div>
+                </div>
+            </form>
+            <form className='mt-5'>
+                <h1 className='text-[25px] font-semibold'>
+                    Drop - Off
+                </h1>
+                <div className=''>
+                    <div className='flex justify-between gap-5'> 
+                        <div className='flex flex-col'> 
+                            <label>Locations</label>
+                            <select id="" className='bg-[#eeee] w-[300px] py-3 px-5 rounded-lg' >
+                                <option value="">Toshkent</option>
+                                <option value="">Tokio</option>
+                                <option value="">Madrid</option>
+                            </select>
+                        </div>
+                        <div className='flex flex-col'>
+                            <label>Date</label>
+                            <input type="date" className='bg-[#eeee] w-[300px] py-3 px-5 rounded-lg' />
+                        </div>
+                    </div>
+                    <div className='flex flex-col'>
+                        <label>Time</label>
+                        <input type="time" className='bg-[#eeee] w-[300px] py-3 px-5 rounded-lg' />
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Step2of4
--- src/pages/rent/RentCard.jsx
+++ src/pages/rent/RentCard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Step1of4 from '../../components/RentComponents/Step1of4'
+import Step2of4 from '../../components/RentComponents/Step2of4'
+import Step3of4 from '../../components/RentComponents/Step3of4'
 
 const RentCard = () => {
   return (
-    <div>RentCard</div>
+    <div className='flex flex-col gap-10'>
+      <Step1of4/>
+      <Step2of4/>
+      <Step3of4/>
+    </div>
   )
 }
 
