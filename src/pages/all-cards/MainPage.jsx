6c3091c61e74a009daa8a7f9d83f8b77153fb2ca
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-01-27 14:27:08 +0500

    ketti

--- src/pages/all-cards/MainPage.jsx
+++ src/pages/all-cards/MainPage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Filter from '../../components/filter-cards/Filter'
 import { Outlet } from 'react-router-dom'
+import LocationData from '../../components/PIck-Up/PickUp'
 const MainPage = () => {
   return (
     <section className='w-full flex justify-between gap-5'>
       <nav className='max-w-[30%]'>
         <Filter />
       </nav>
-      <section className='max-w-[70%]'>
-      <Outlet />
+      <section className='max-w-[85%]'>
+        <LocationData />
+        <Outlet />
       </section>
     </section>
   )
